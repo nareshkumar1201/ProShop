e0a53fadd4de92668d1bb11f79bca0dfda21023a
sachin-tele-t20 <sachin.tele.t20@gmail.com>
2024-07-06 19:52:32 +0530

    added frontend folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
+import HomeScreen from "./screens/HomeScreen";
 const App = () => {
-  return <div>its my app</div>;
+  return (
+    <>
+      <div>
+        <Header />
+        <HomeScreen />
+        <Footer />
+      </div>
+    </>
+  );
 };
 
 export default App;

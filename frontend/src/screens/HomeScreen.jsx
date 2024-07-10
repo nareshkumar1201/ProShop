2422b39fbc857db2cac6d08757910278815f84a9
sachin-tele-t20 <sachin.tele.t20@gmail.com>
2024-07-10 18:51:24 +0530

    added product page

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,14 +1,15 @@
 import { Row, Col } from "react-bootstrap";
-import Products from "../products";
-import ProductScreen from "./ProductScreen";
+import ProductsData from "../products";
+import Product from "../components/Product";
+
 const HomeScreen = () => {
   return (
     <>
       <h1>Latest Products </h1>
       <Row>
-        {Products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
-            <ProductScreen product={product} />
+        {ProductsData.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,23 +1,69 @@
-import { Card } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
-
-const ProductScreen = ({ product }) => {
+import { Row, Col, ListGroup, Card, Button, Image } from "react-bootstrap";
+import Rating from "../components/Ratings";
+import ProductsData from "../products";
+const ProductScreen = () => {
+  const { id: productId } = useParams();
+  const product = ProductsData.find((p) => p._id === productId);
+  console.log(product.name);
   return (
-    <>
-      <Card className="my-3 p-3 rounded">
-        <Link to={`/product/${product._id}`}>
-          <Card.Img src={product.image} variant="top" />
-        </Link>
-        <Card.Body>
-          <Link to={`/product\${product._id}`}>
-            <Card.Title as="div">
-              <strong>{product.name}</strong>
-            </Card.Title>
-          </Link>
-          <Card.Text as="h3">${product.price}</Card.Text>
-        </Card.Body>
-      </Card>
-    </>
+    <div>
+      <Link to="/">
+        <Button className="btn btn-light my-3">Back</Button>
+      </Link>
+      <Row>
+        <Col md={5}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={4}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price {product.price}</ListGroup.Item>
+            <ListGroup.Item>Description :{product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <ListGroup>
+            <ListGroup.Item>
+              <Row>
+                <Col>Price :</Col>
+                <Col>
+                  <strong> &#8377; {`${product.price}`}</strong>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Row>
+                <Col>status :</Col>
+                <Col>
+                  <strong>
+                    {product.countInStock > 0 ? "In Stock" : "Out of Stock "}
+                  </strong>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                className="btn-block"
+                type="button"
+                disabled={product.countInStock === 0}
+              >
+                Add to Cart
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
